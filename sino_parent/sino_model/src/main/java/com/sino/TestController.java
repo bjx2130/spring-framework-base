d79d19c6bdb3a5a69a7883d5b3659c69b4661a12
baojx <308110158@qq.com>
2020-05-18 10:38:27 +0800

    修改代码结构

--- sino_parent/sino_model/src/main/java/com/sino/TestController.java
+++ sino_parent/sino_model/src/main/java/com/sino/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-public class Test {
+public class TestController {
 
 	
 	@Autowired
